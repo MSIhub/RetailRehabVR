2b12d688fef6e0f6a041bf81a11af34f76c3f0ab
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-10-04 23:37:49 +0200

    Supermarket setup completed with grabbable objects stocked. Lighting, trolley setup with custom colliders added.

---
+++ Assets/Scripts/MarketStockItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketStockItem: MonoBehaviour
+{
+    public int itemCode;
+    public float cost;
+    
+}
